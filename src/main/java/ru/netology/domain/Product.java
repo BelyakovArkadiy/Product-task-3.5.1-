221d6657aa853526fcdac3a83f83a0eddb2664a6
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-02-08 19:06:59 +0600

    added Mockito/update TestManager

--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -5,12 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 
 public class Product {
     private int id;
     private String name;
     private int price;
+
+
 }
--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -1,14 +1,16 @@
 package ru.netology.manager;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
 import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductRepository;
 @Data
-@EqualsAndHashCode
+
 
 public class ProductManager {
     private ProductRepository repository;
@@ -22,6 +24,7 @@ public class ProductManager {
         for (Product product : repository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
+                // используйте System.arraycopy, чтобы скопировать всё из result в tmp
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = product;
                 result = tmp;
